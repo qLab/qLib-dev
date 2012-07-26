3f815fb363bf3a4cd1959a5cbed0d3053c4ac16f
Imre Tuske <imre.tuske@gmail.com>
2012-07-26 22:56:45 +0200

    support script: opalias

---
+++ scripts/opalias_ql.cmd
@@ -0,0 +1,18 @@
+#
+# opaliases for qLib
+#
+# this file is intended for assets that are initially, but named properly later.
+#
+# if your scene complains about not findind qLib assets, source this file
+# and reopen your .hip file.
+#
+#
+
+# misspelled asset names
+#
+opalias Sop qLib::proxy_primgroups_ql::1 proxy_primgroups
+
+
+# ...
+#
+
